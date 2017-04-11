eead08aab1f166b69a33e9d370197b394b47f7f9
finger-numb <teotoplak95@gmail.com>
2017-04-11 11:51:09 +0200

    Created panel for adding swtYears

--- public/javascripts/SWTYearSetup.js
+++ public/javascripts/SWTYearSetup.js
@@ -4,6 +4,7 @@ var addButton = document.getElementById("addButton");
 var addAgency = document.getElementById("addAgency");
 var addCountry = document.getElementById("addCountry");
 
+
 //year selector
 var current = new Date().getFullYear();
 for(var i=0; i<=15; i++) {
@@ -17,30 +18,46 @@ addYear.style.minWidth = "80px";
 
 addButton.addEventListener("click", function(){
 var addingYear = addYear.options[addYear.selectedIndex].value;
-    alert(table.rows.length)
-if(table.rows.length == 0) {
-    addYearToRow(0);
+if(table.rows.length == 2) {
+    addYearToRow(1);
     return;
 }
 var lastYear;
-    for (var i = 0, row; row = table.rows[i]; i++) {
-        lastYear = row.cells[0].value;
-        if(lastYear <= addingYear) {
+    for (var i = 1; i < table.rows.length-1; i++) {
+        var row = table.rows[i];
+        lastYear = row.cells[0].innerHTML;
+        if(lastYear == addingYear) {
+            alert("You already added that year!");
+            return;
+        }
+        if(lastYear < addingYear) {
             addYearToRow(i);
+            return;
         }
     }
-
+    addYearToRow(table.rows.length-1);
 
 });
 
 
 function addYearToRow(i) {
     var row = table.insertRow(i);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(3);
-    cell1.innerHTML = addYear.value;
+    var cell1 = row.insertCell(-1);
+    var cell2 = row.insertCell(-1);
+    var cell3 = row.insertCell(-1);
+    var cell4 = row.insertCell(-1);
+    cell1.innerHTML = addYear.options[addYear.selectedIndex].value;
     cell2.innerHTML = addAgency.value;
     cell3.innerHTML = addCountry.value;
-}
+    var deleteButton = document.createElement("button");
+    deleteButton.className = "btn btn-danger";
+    deleteButton.innerHTML = "<span class=' glyphicon glyphicon-minus'></span>";
+    deleteButton.addEventListener("click", function(){
+        var rowToDelete = deleteButton.parentNode.parentNode;
+        rowToDelete.parentNode.removeChild(row);
+    });
+    cell4.appendChild(deleteButton);
 
+    addAgency.value = "";
+    addCountry.value = "";
+}

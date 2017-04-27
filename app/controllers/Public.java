5517ddddf8bc9be540d0c274542764dd34d5bdc4
TT95 <teotoplak95@gmail.com>
2017-04-28 00:16:33 +0200

    Restoring deleted controller Public

---
+++ app/controllers/Public.java
@@ -0,0 +1,44 @@
+package controllers;
+
+import org.pac4j.play.java.Secure;
+import org.slf4j.Logger;
+import play.data.Form;
+import play.data.FormFactory;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.*;
+
+import javax.inject.Inject;
+
+/**
+ * Pages visible to all users
+ * Created by teo on 11/28/16.
+ */
+public class Public extends Controller {
+
+    private Logger logger = org.slf4j.LoggerFactory.getLogger(getClass());
+
+    @Inject
+    private FormFactory formFactory;
+
+
+    public Result landing() {
+        return ok(landing.render());
+    }
+
+
+//    @Secure(clients = "FacebookClient", authorizers = "custom")
+//    public Result login() {
+//        flash("logged in!!");
+//        return redirect(request().getHeader("referer"));
+//    }
+//
+//    private Result loginError(Form<User> boundForm) {
+//        flash("error", "Incorrect login!");
+//        return badRequest(views.html.login.render(boundForm));
+//    }
+
+
+
+
+}
